d90a9a7bc6912cb170a89e20334b5f5c2fcb009b
BensaltanaHoussam <bensaltna07@gmail.com>
2024-11-27 01:29:29 +0100

    adding a pop up modal to let the user type information to add player

--- script.js
+++ script.js
@@ -19,3 +19,61 @@
 
 
 
+// pop up-------------
+
+  document.addEventListener("DOMContentLoaded", function () {
+
+    const addButtons = document.querySelectorAll("#addPlayer");
+    const playerModal = document.getElementById("playerModal");
+    const closeModalButton = document.getElementById("closeModal");
+    const playerForm = document.getElementById("playerForm");
+
+    
+    function openModal(event) {
+        event.preventDefault(); 
+        playerModal.classList.remove("hidden");
+    }
+
+ 
+    function closeModal() {
+        playerModal.classList.add("hidden");
+    }
+
+
+    for (let i = 0; i < addButtons.length; i++) {
+        addButtons[i].addEventListener("click", openModal);
+    }
+
+   
+ 
+    closeModalButton.addEventListener("click", closeModal);
+
+    
+    playerForm.addEventListener("submit", function (event) {
+
+        
+        event.preventDefault(); 
+        
+        
+        const playerData = {
+            fullName: document.getElementById("fullName").value,
+            position: document.getElementById("position").value,
+            club: document.getElementById("club").value,
+            country: document.getElementById("country").value,
+            stats: {
+                pac: document.getElementById("pac").value,
+                sho: document.getElementById("sho").value,
+                pas: document.getElementById("pas").value,
+                dri: document.getElementById("dri").value,
+                def: document.getElementById("def").value,
+                phy: document.getElementById("phy").value,
+            }
+        };
+
+        closeModal();
+    });
+});
+  
+
+
+  
